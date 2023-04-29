106723993857aaaedbdccfa26aa54896e2024df5
Tacho <mekrtchian@gmail.com>
2023-04-29 14:55:19 +0200

    Popup window appearance

--- scripts/app.js
+++ scripts/app.js
@@ -17,6 +17,9 @@ const page = {
     content: {
         daysContainer: document.getElementById('days'),
         nextDay: document.querySelector('.habbit__day')
+    },
+    popup: {
+        index: document.getElementById('add-habbit_popup')
     }
 }
 
@@ -34,6 +37,16 @@ function loadData() {
 function saveData() {
     localStorage.setItem(HABBIT_KEY, JSON.stringify(habbits));
 }
+
+
+function togglePopup() {
+    if (page.popup.index.classList.contains('cover_hidden')) {
+        page.popup.index.classList.remove('cover_hidden')
+    } else {
+        page.popup.index.classList.add('cover_hidden')
+    }
+}
+
 
 // render
 
